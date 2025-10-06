e502c472f0c7def1a486f9a11f3c7db92b12595b
NøCrari <mamonjyrivo@gmail.com>
2025-10-06 11:13:22 +0200

    Delete repos_github/cloud-design directory

--- repos_github/cloud-design/Dockerfiles/inventory-db/initdb/00-readme.sql
+++
@@ -1,4 +0,0 @@
--- Inventory DB init placeholder
--- The database itself will be created via POSTGRES_DB env.
--- Tables are created by the application on first run.
-
--- repos_github/cloud-design/infra/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "gke_name" { value = google_container_cluster.this.name }
-output "gke_location" { value = google_container_cluster.this.location }
-output "artifact_registry_repo" { value = google_artifact_registry_repository.docker.repository_id }
-output "cloudsql_instance" {
-  value = var.enable_cloud_sql ? google_sql_database_instance.pg[0].name : ""
-}
-output "api_gateway_ingress_ip" { value = google_compute_global_address.api_gateway_ingress.address }
